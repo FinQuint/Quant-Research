bde24841e0b3dad52ebb5479dd93d246dcafc316
Yunan Xie <80860032+walleva1715@users.noreply.github.com>
2026-08-23 17:24:47 -0400

    Add cash flow functions to module exports

--- src/finquint/fixed_income/__init__.py
+++ src/finquint/fixed_income/__init__.py
@@ -1,7 +1,17 @@
 from .bond import Bond
+from .cashflows import cash_flows, generate_cashflows
 from .pricing import price_bond
 from .risk import convexity, dv01, macaulay_duration, modified_duration
 from .yield_metrics import yield_to_maturity
 
-__all__ = ["Bond", "price_bond", "yield_to_maturity", "macaulay_duration", "modified_duration", "dv01", "convexity"]
-
+__all__ = [
+    "Bond",
+    "cash_flows",
+    "generate_cashflows",
+    "price_bond",
+    "yield_to_maturity",
+    "macaulay_duration",
+    "modified_duration",
+    "dv01",
+    "convexity",
+]

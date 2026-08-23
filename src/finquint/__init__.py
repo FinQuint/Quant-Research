a9896165ac5787a8849c08fe40471857b9ed6c8c
Yunan Xie <80860032+walleva1715@users.noreply.github.com>
2026-08-23 17:24:10 -0400

    Add version number to __init__.py

--- src/finquint/__init__.py
+++ src/finquint/__init__.py
@@ -1,17 +1,3 @@
-from .bond import Bond
-from .cashflows import cash_flows, generate_cashflows
-from .pricing import price_bond
-from .risk import convexity, dv01, macaulay_duration, modified_duration
-from .yield_metrics import yield_to_maturity
+"""FinQuint quantitative research framework."""
 
-__all__ = [
-    "Bond",
-    "cash_flows",
-    "generate_cashflows",
-    "price_bond",
-    "yield_to_maturity",
-    "macaulay_duration",
-    "modified_duration",
-    "dv01",
-    "convexity",
-]
+__version__ = "0.3.0"
